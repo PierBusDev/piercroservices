c2578227f4b2a3d45f9ee75bba8b4932ec9a9780
Pierpaolo Busacchi <pierbus.dev@gmail.com>
2022-09-03 21:08:06 +0200

    now when authenticating the authenticate service will call the logger service to log the action

--- authentication/cmd/api/handlers.go
+++ authentication/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -29,7 +31,14 @@ func (c *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		c.errorJSON(w, errors.New("Invalid Credentials"), http.StatusBadRequest)
 		return
 	}
-	
+
+	//logging authentication
+	err = c.logRequest("authentication", fmt.Sprintf("User %s authenticated", user.Email))
+	if err != nil {
+		c.errorJSON(w, err, http.StatusInternalServerError)
+		return
+	}
+	//creating and sending back response
 	payload := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Successfully authenticated user: %s", user.Email),
@@ -37,4 +46,33 @@ func (c *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (c *Config) logRequest(name, data string) error {
+	entry := struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}{
+		Name: name,
+		Data: data,
+	}
+
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	logServiceUrl := "http://logger-service/log"
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
